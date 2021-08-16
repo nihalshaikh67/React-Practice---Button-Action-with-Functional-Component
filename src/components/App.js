8150b175913be9233f4535a0f6cf9d7e36a7a633
nihalshaikh67 <nihalshaikh834@gmail.com>
2021-08-16 12:36:05 +0530

    react

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,15 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [state,setState] = useState(false);
+
+  const clicked = () =>{
+    setState(true);
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+     <button id="click" onClick = {clicked}>Click me</button>
+     {state && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     </div>
   );
 }
